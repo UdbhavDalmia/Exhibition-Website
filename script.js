bf5958c1bc085de623d17f3763a74dd2853f3df7
UdbhavDalmia <129969503+UdbhavDalmia@users.noreply.github.com>
2024-11-16 11:27:58 +0000

    new files

---
+++ script.js
@@ -0,0 +1,7 @@
+function navigateToLink() {
+    const dropdown = document.getElementById("dropdown");
+    const selectedValue = dropdown.value;
+    if (selectedValue) {
+      window.location.href = selectedValue; // Redirects to the selected link
+    }
+  }
